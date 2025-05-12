0febb385463db9ac2810ebb6955b3c2ed655363a
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-05-12 19:54:06 -0300

    feat: implement SSS distribution and recovery function prototypes

---
+++ sss_algos.c
@@ -0,0 +1,21 @@
+#include "sss_algos.h"
+
+BMPImageT **sss_distribute_8(BMPImageT *image, uint32_t k, uint32_t n) {
+    fprintf(stderr, "sss_distribute_8 not implemented\n");
+    return NULL;
+}
+
+BMPImageT **sss_distribute_generic(BMPImageT *image, uint32_t k, uint32_t n) {
+    fprintf(stderr, "sss_distribute_generic not implemented\n");
+    return NULL;
+}
+
+BMPImageT *sss_recover_8(BMPImageT **shadows, uint32_t k) {
+    fprintf(stderr, "sss_recover_8 not implemented\n");
+    return NULL;
+}
+
+BMPImageT *sss_recover_generic(BMPImageT **shadows, uint32_t k) {
+    fprintf(stderr, "sss_recover_generic not implemented\n");
+    return NULL;
+}
---
+++ sss_algos.h
@@ -0,0 +1,14 @@
+#ifndef _SSS_ALGOS_H
+#define _SSS_ALGOS_H
+#include "bmp.h"
+
+typedef BMPImageT **(*DistributeFnT)(BMPImageT *image, uint32_t k, uint32_t n);
+typedef BMPImageT *(*RecoverFnT)(BMPImageT **shadows, uint32_t k);
+
+BMPImageT **sss_distribute_8(BMPImageT *image, uint32_t k, uint32_t n);
+BMPImageT **sss_distribute_generic(BMPImageT *image, uint32_t k, uint32_t n);
+
+BMPImageT *sss_recover_8(BMPImageT **shadows, uint32_t k);
+BMPImageT *sss_recover_generic(BMPImageT **shadows, uint32_t k);
+
+#endif
